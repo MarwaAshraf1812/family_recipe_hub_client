8a5d9caccb08339ec4310e7520882fb62974c6c5
Amr Khaled <amrk6965@gmail.com>
2024-12-14 05:08:59 -0800

    Downloading some things

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,12 @@
-import Login from './Pages/Auth/Login';
+// import Login from './Pages/Auth/Login';
+// import { Button } from "@material-tailwind/react";
+// import { Swiper, SwiperSlide } from "swiper/react";
+// import "swiper/css";
 export default function App() {
   return (
     <div>
-      <Login />
+      {/* <Login /> */}
+      {/* <Button>Button</Button> */}
     </div>
   );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,16 +1,14 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-    darkMode: ['class'],
-    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+import withMT from "@material-tailwind/react/utils/withMT";
+export default withMT({
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-  	extend: {
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {}
-  	}
+    extend: {},
   },
   plugins: [import('tailwindcss-animate')],
-}
+});
+
