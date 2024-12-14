b57edf6d3cb67842ed2c5323c7de51cb0e89e841
MarwaAshraf1812 <marashraf090@gmail.com>
2024-12-14 22:20:02 +0200

    feat: implement home and navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
-
-import Navbar from './components/layout/Navbar/Navbar';
 import Landing from './Pages/Landing';
 import './App.css';
+import Navbar from './layout/Navbar/Navbar';
 
 export default function App() {
   return (
