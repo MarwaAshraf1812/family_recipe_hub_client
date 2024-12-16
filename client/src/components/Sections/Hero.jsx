ad9a0a3fbd9da43847626db3985b01deb2ed37f2
MarwaAshraf1812 <marashraf090@gmail.com>
2024-12-16 22:53:14 +0200

    feat: fix hero section

--- client/src/components/Sections/Hero.jsx
+++ client/src/components/Sections/Hero.jsx
@@ -64,7 +64,7 @@ const slideData = [
 
 export default function Hero() {
   return (
-    <div className="px-11 py-11 mx-auto">
+    <div className="px-4 py-10 md:px-11 md:py-11 mx-auto">
       <Swiper
         modules={[Pagination, A11y, Autoplay]}
         spaceBetween={20}
@@ -74,58 +74,56 @@ export default function Hero() {
           delay: 7000,
           disableOnInteraction: false,
         }}
-        className="h-[700px]"
+        className="h-[500px] md:h-[700px]"
       >
         {slideData.map((slide, index) => (
           <SwiperSlide key={index}>
-            <div className="flex h-full rounded-full px-28">
-
-              <div className="w-1/2 h-full bg-main_color flex items-center p-5 relative group">
-                <div className="flex flex-col">
+            <div className="flex flex-col md:flex-row h-full rounded-md overflow-hidden bg-gray-100 shadow-lg">
+              <div className="md:w-1/2 h-full bg-main_color flex items-center p-5 md:p-10 relative group">
+                <div className="flex flex-col w-full">
                   <div
-                    className="flex bg-white px-8 py-2 rounded-full items-center absolute top-6 left-6 
+                    className="flex bg-white px-4 rounded-full py-2 items-center absolute top-6 left-6 
                     transform transition-transform duration-500 group-hover:scale-110"
                   >
                     <img
                       src={slide.badge.image}
                       alt={slide.badge.text}
-                      className="mr-4 w-8 h-8 object-cover"
+                      className="mr-4 w-6 h-6 md:w-8 md:h-8 object-cover"
                     />
-                    <p className="font-semibold text-sm">{slide.badge.text}</p>
+                    <p className="font-semibold text-xs md:text-sm">{slide.badge.text}</p>
                   </div>
 
-                  <div className="flex flex-col">
-                    <h1 className="text-7xl font-bold mb-10 group-hover:text-gray-900">
-                      {slide.title}
-                    </h1>
-                    <p className="text-gray-700 mb-4">{slide.description}</p>
+                  <h1 className="text-3xl md:text-5xl lg:text-7xl font-bold mb-4 md:mb-8 lg:mt-0 group-hover:text-gray-900">
+                    {slide.title}
+                  </h1>
+                  <p className="text-gray-700 text-sm md:text-base mb-4">{slide.description}</p>
+
+                  <div className="flex gap-2 md:gap-4 items-center mb-6">
+                    <div className="flex bg-gray-300 p-3 md:p-4 cursor-pointer rounded-full items-center hover:bg-slate-300 transition-colors duration-300">
+                      <MdTimer className="mr-2 text-base md:text-lg" />
+                      <p className="text-xs md:text-sm font-medium">{slide.time}</p>
+                    </div>
+                    <div className="flex bg-gray-300 p-3 md:p-4 cursor-pointer rounded-full items-center hover:bg-slate-300 transition-colors duration-300">
+                      <ImSpoonKnife className="mr-2 text-base md:text-lg" />
+                      <p className="text-xs md:text-sm font-medium">{slide.type}</p>
+                    </div>
                   </div>
 
-                  <div className="flex gap-4 items-center mb-6">
-                    <div className="flex bg-slate-200 p-4 cursor-pointer rounded-full items-center hover:bg-slate-300 transition-colors duration-300">
-                      <MdTimer className="mr-2 text-lg" />
-                      <p className="text-sm font-medium">{slide.time}</p>
-                    </div>
-                    <div className="flex bg-slate-200 p-4 cursor-pointer rounded-full items-center hover:bg-slate-300 transition-colors duration-300">
-                      <ImSpoonKnife className="mr-2 text-lg"/>
-                      <p className="text-sm font-medium">{slide.type}</p>
-                    </div>
-                  </div>
-                  <div className="flex items-center justify-between absolute left-0 bottom-8 w-full p-5">
+                  <div className="flex items-center justify-between  absolute left-0 bottom-8  w-full p-5 lg:mt-0 mt-20">
                     <div className="flex gap-3 items-center">
                       <img
                         src={slide.author.image}
                         alt={slide.author.name}
-                        className="rounded-full w-12 h-12 object-cover hover:scale-105 transition-transform duration-300"
+                        className="rounded-full w-10 h-10 md:w-12 md:h-12 object-cover hover:scale-105 transition-transform duration-300"
                       />
                       <div className="flex flex-col">
-                        <h5 className="font-bold text-sm">{slide.author.name}</h5>
+                        <h5 className="font-bold text-xs md:text-sm">{slide.author.name}</h5>
                         <p className="text-gray-500 text-xs">{slide.author.date}</p>
                       </div>
                     </div>
-                    <button className="bg-black text-white px-9 py-5 rounded-3xl font-medium text-lg flex items-center hover:bg-black/80">
+                    <button className="bg-black text-white px-6 py-3 md:px-9 md:py-5 rounded-3xl font-medium text-sm md:text-lg flex items-center hover:bg-black/80">
                       Recipes
-                      <div className="rounded-3xl ml-4">
+                      <div className="rounded-3xl ml-2 md:ml-4">
                         <RiVideoFill />
                       </div>
                     </button>
@@ -133,11 +131,11 @@ export default function Hero() {
                 </div>
               </div>
 
-              <div className="w-1/2 h-full">
+              <div className="md:w-1/2 h-full overflow-hidden hidden md:block">
                 <img
                   src={slide.image}
                   alt={slide.title}
-                  className="w-full h-full object-cover rounded-lg"
+                  className="w-full h-full object-cover"
                 />
               </div>
             </div>
@@ -147,3 +145,4 @@ export default function Hero() {
     </div>
   );
 }
+
