b0ad12fa003588325a3b247bc44cb87c38526159
MarwaAshraf1812 <marashraf090@gmail.com>
2024-12-17 00:48:36 +0200

    feat: implement categories section

--- client/src/Pages/Landing.jsx
+++ client/src/Pages/Landing.jsx
@@ -1,7 +1,12 @@
+import Categories from "../components/Sections/Categories/Categories";
 import Hero from "../components/Sections/Hero";
 
 export default function Landing() {
     return (
+      <>
       <Hero />
+      <Categories />
+      </>
+      
     )
   }
---
+++ client/src/components/Sections/Categories/Categories.jsx
@@ -0,0 +1,40 @@
+import { Link } from 'react-router-dom';
+import CategoryItem from './CategoryItem';
+import BreakFastImage from '../../../assets/categories/im6.png';
+import VeganImage from '../../../assets/categories/im1.png';
+import MeatImage from '../../../assets/categories/im2.png';
+import DesertImage from '../../../assets/categories/im3.png';
+import LunchImage from '../../../assets/categories/im4.png';
+import ChocoImage from '../../../assets/categories/im5.png';
+
+export default function Categories() {
+  const categories = [
+    { image: BreakFastImage, title: 'Breakfast', bgColor: 'bg-linear_black' },
+    { image: VeganImage, title: 'Vegetables', bgColor: 'bg-linear_green' },
+    { image: MeatImage, title: 'Dairy', bgColor: 'bg-linear_red' },
+    { image: DesertImage, title: 'Desert', bgColor: 'bg-linear_yellow' },
+    { image: LunchImage, title: 'Lunch', bgColor: 'bg-linear_grey' },
+    { image: ChocoImage, title: 'Chocolate', bgColor: 'bg-linear_black' },
+  ];
+
+  return (
+    <div className="w-full h-full py-16 px-10 md:px-14">
+      <div className="flex justify-between items-center mb-10 md:mb-14 w-full">
+        <h1 className="text-4xl font-bold text-black md:text-5xl">Categories</h1>
+        <Link to="/categories" className="text-black hover:bg-main_color shadow-md bg-main_color p-4 rounded-lg font-semibold">
+          View All Categories
+        </Link>
+      </div>
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-6 w-full">
+        {categories.map((category, index) => (
+          <CategoryItem
+            key={index}
+            image={category.image}
+            title={category.title}
+            bgColor={category.bgColor}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
