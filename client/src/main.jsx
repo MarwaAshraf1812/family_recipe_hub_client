4f4d4bfa3d7962d974b0dc11e3b08ab10d6da3ab
Amr Khaled <162748788+AmrKhaled35@users.noreply.github.com>
2024-12-16 16:07:01 -0800

    Revert "Feat:Home Page& Auth"

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Provider } from 'react-redux'
-import { store } from './context/Redux/store'
 import './index.css'
 import AppRouters from './routes/AppRoutes'
-import { ThemeProvider } from '@material-tailwind/react'
+import { ThemeProvider } from "@material-tailwind/react";
 createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <StrictMode>
-      <ThemeProvider>
+  <StrictMode>
+    <ThemeProvider>
         <AppRouters />
-      </ThemeProvider>
-    </StrictMode>
-  </Provider>
+    </ThemeProvider>
+  </StrictMode>
 )
