7aa035b10e763fbdc33eb5809a6a7efd0f0b3ba6
MarwaAshraf1812 <marashraf090@gmail.com>
2025-01-30 19:34:44 +0200

    add secret key to the env

--- server/family_recipe_hub/settings.py
+++ server/family_recipe_hub/settings.py
@@ -24,7 +24,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&_d)kg@)r6u1@3r@acx%!25#c^h^wt-gc3uzu@=2$kh9xtfa@('
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
